b73944b014d5c0dd791ebf7b763324f6b2dce3b6
4urbanoff <4urbanoff@gmail.com>
2014-10-10 11:52:47 +0400

    init two

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,12 +1,15 @@
+#require 'uglifier'
+#require 'yui/compressor'
+
 Nali::Application.configure :development do |config|
   
 # config.assets_digest = true
   
   config.assets_debug = true
   
-# config.assets.js_compressor  = Uglifier.new( mangle: true )
-  
-# config.assets.css_compressor = YUI::CssCompressor.new
+#  config.assets.js_compressor  = Uglifier.new( mangle: true )
+#  
+#  config.assets.css_compressor = YUI::CssCompressor.new
   
   ActiveRecord::Base.logger = false #Logger.new STDOUT 
  
