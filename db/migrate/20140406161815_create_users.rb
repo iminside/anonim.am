8c610c7b85f46d075516d7786e93365de365d392
4urbanoff <4urbanoff@gmail.com>
2014-12-01 15:04:11 +0300

    update column limits

--- db/migrate/20140406161815_create_users.rb
+++ db/migrate/20140406161815_create_users.rb
@@ -2,9 +2,9 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string  :token,  limit: 32
-      t.string  :name
-      t.string  :gender
-      t.string  :color
+      t.string  :name,   limit: 100
+      t.string  :gender, limit: 5
+      t.string  :color,  limit: 10
       t.integer :image,  default: 0
       t.integer :search, default: 1, limit: 1
       t.boolean :online, default: false
